6daf230852224f7af82c983eaf9e6c989ddbb898
Ivy Mic <ibrahim_e@hotmail.co.uk>
2018-08-16 12:03:08 +0100

    user has a hobby

--- directory.rb
+++ directory.rb
@@ -1,16 +1,18 @@
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_students(students)
+  counter = 0
+  while counter < students.length
+    puts "#{counter + 1}. #{students[counter][:name]}, their hobby is: #{students[counter][:hobby]} (#{students[counter][:cohort]} cohort)".center(100)
+    counter += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(100)
 end
 
 def input_students
@@ -19,14 +21,36 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty?
-    students << {name: name, cohort: :november}
+    puts "What is #{name}'s hobby: "
+    hobby = retrieve_response
+    puts "And which cohort do they belong to (just hit return if you dont know): "
+    cohort = retrieve_response
+    cohort = :november if cohort == nil
+    students << {name: name, cohort: cohort, hobby: hobby}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
   students
 end
 
+def retrieve_response
+  response = gets.chomp
+  puts "type yes to confirm, or no to start again"
+  yes_or_no_response = gets.chomp
+  if yes_or_no_response == "yes"
+    return response
+  else
+    retrieve_response.to_sym
+  end
+end
+# def input_letter
+#   puts "Please enter the letter of students you would like to see: "
+#   input = gets.chomp
+#   input[0]
+# end
+
 students = input_students
 print_header
-print(students)
+# letter = input_letter
+print_students(students)
 print_footer(students)
