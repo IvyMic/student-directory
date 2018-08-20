8b8a70949e6bc67e94154d0a0248d796b9d7c383
Ivy Mic <ibrahim_e@hotmail.co.uk>
2018-08-20 10:42:04 +0100

    removedd some features to match tutorial

--- directory.rb
+++ directory.rb
@@ -1,80 +1,65 @@
-def print_header
-  puts "The students of Villains Academy".center(100)
-  puts "-------------".center(100)
-end
-
-def print_students_list
-  counter = 0
-  while counter < @students.length
-    puts "#{counter + 1}. #{@students[counter][:name]}, their hobby is: #{@students[counter][:hobby]} (#{@students[counter][:cohort]} cohort)".center(100)
-    counter += 1
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students".center(100)
-end
+@students = [] # an empty array accessible to all methods
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+  # get the first name
   name = gets.chomp
-  while !name.empty?
-    puts "What is #{name}'s hobby: "
-    hobby = retrieve_response
-    puts "And which cohort do they belong to (just hit return if you dont know): "
-    cohort = retrieve_response
-    cohort = :november if cohort == ""
-    @students << {name: name, cohort: cohort, hobby: hobby}
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = gets.chomp
   end
 end
 
-def retrieve_response
-  response = gets.chomp
-  puts "type yes to confirm, or no to start again"
-  yes_or_no_response = gets.chomp
-  if yes_or_no_response == "yes"
-    return response
-  else
-    retrieve_response.to_sym
-  end
-end
-
 def interactive_menu
-  @students = []
   loop do
     print_menu
     process(gets.chomp)
   end
 end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-  end
-end
-
 def print_menu
-  # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit" # 9 because we'll be adding more items
-  # 2. read the input and save it into a variable
 end
 
 def show_students
   print_header
-  print_students_list
+  print_student_list
   print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
