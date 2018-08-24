1569fde941ce6e702f5eb6c05efb9d7ecbf7192c
Ivy Mic <ibrahim_e@hotmail.co.uk>
2018-08-24 11:42:36 +0100

    completed exercises

--- directory.rb
+++ directory.rb
@@ -3,8 +3,8 @@
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list"
+  puts "4. Load the list"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -18,18 +18,42 @@ end
 def process(selection)
   case selection
   when "1"
+    puts "Please input new students"
+    sleep(1)
     input_students
   when "2"
+    puts "Showing students"
+    sleep(1)
     show_students
   when "9"
+    puts "Exiting..."
+    sleep(3)
     exit # this will cause the program to terminate
   when "3"
+    puts "Saving students"
+    sleep(1)
     save_students
+    puts "File saved!"
   when "4"
-    load_students
+    filename = retrieve_filename
+    if File.exists?(filename) == false
+      puts "file not found0"
+      return
+    end
+    puts "Loading students..."
+    sleep(2)
+    load_students(filename)
+    puts "Students loaded. choose show students option to show them"
   else
     puts "I don't know what you meant, try again"
   end
+end
+
+def retrieve_filename
+  puts "Enter file name of students you would like to load:"
+  filename = gets.chomp
+  filename = "#{filename}.csv"
+  filename
 end
 
 def input_students
@@ -39,11 +63,32 @@ def input_students
   name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
+    puts "What is #{name}'s hobby:"
+    hobby = retrieve_response
+    puts "And which cohort do they belong to (just hit return if you dont know): "
+    cohort = retrieve_response
+    cohort = cohort.to_sym
+    cohort = :november if cohort.empty?
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    add_hash_to_students({name: name, cohort: cohort, hobby: hobby})
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
+  end
+end
+
+def add_hash_to_students(hash)
+  @students << hash
+end
+
+def retrieve_response
+  response = STDIN.gets.chomp
+  puts "type yes to confirm, or no to type it again"
+  yes_or_no_response = STDIN.gets.chomp
+  if yes_or_no_response == "yes"
+    return response
+  else
+    retrieve_response
   end
 end
 
@@ -54,26 +99,31 @@ def show_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
 def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  counter = 0
+  while counter < @students.length
+    puts "#{counter + 1}. #{@students[counter][:name]}, their hobby is: #{@students[counter][:hobby]} (#{@students[counter][:cohort]} cohort)".center(100)
+    counter += 1
   end
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students"
+  puts "Overall, we have #{@students.count} great students".center(100)
 end
 
 def save_students
   # open the file for writing
-  file = File.open("students.csv", "w")
+  puts "What shall you name the file (dont type the extension, just the file name)"
+  filename = retrieve_response
+  filename = filename.gsub(/\s+/, "")
+  file = File.open("#{filename}.csv", "w")
   # iterate over the array of students
   @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
+    student_data = [student[:name], student[:cohort], student[:hobby]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
@@ -83,8 +133,8 @@ end
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    name, cohort, hobby = line.chomp.split(',')
+    add_hash_to_students({name: name, cohort: cohort.to_sym, hobby: hobby})
   end
   file.close
 end
